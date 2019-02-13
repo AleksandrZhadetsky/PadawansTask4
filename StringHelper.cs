9a22ff280e2730c651632d88965e62babc45e794
Alexander <coolweak2@gmail.com>
2019-02-13 19:03:23 +0300

    Update StringHelper.cs

--- StringHelper.cs
+++ StringHelper.cs
@@ -6,8 +6,25 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException();
+            }
+            int count = 0;
+            char[] pattern = { 'a', 'e', 'u', 'i', 'o' };
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (str[i] == pattern[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
         }
     }
 }
